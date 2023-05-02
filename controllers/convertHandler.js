9574a017dbf79ad937d5b3af7a1c74839d7d8053
David <dhalls@cmnhospitals.org>
2023-05-01 21:55:28 -0600

    firstUpload

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,27 +1,59 @@
+const { init } = require("../server");
+
+function numSplitter(input){
+  let num = input.match(/[.\d\/]+/g) || ["1"];  
+  // console.log(num);
+  let unit = input.match(/[a-zA-Z]+/g)[0];
+  return [num[0], unit];
+}
+
+function checkDiv(posFraction){
+  let nums = posFraction.split("/");
+  if(nums.length > 2){return false;}
+  return nums;
+}
+
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
+  let real = ['km','mi','gal','l','lbs','kg'];
+  let change = ['mi','km','L','gal','kg','lbs'];
+  let inUnit = ['kilometers','miles','gallons','liters','pounds','kilograms'];
+  let outUnit = ['miles','kilometers','liters','gallons','kilograms','pounds'];
+  let index = 0;
+  this.getNum = function(input) {  
+    let res = numSplitter(input)[0];  
+    let nums = checkDiv(res);        
     
-    return result;
+    if(!nums){return undefined;}
+
+    let n1 = nums[0];
+    let n2 = nums[1] || "1";
+
+    res = parseFloat(n1) / parseFloat(n2);
+
+    if(isNaN(n1) || isNaN(n2)){return undefined;}
+    return res; 
   };
   
   this.getUnit = function(input) {
-    let result;
+    let res = numSplitter(input)[1].toLowerCase();
+    this.spellOutUnit(res);
+    if(real.indexOf(res) !== -1){
+      if(res == 'l'){res = 'L';}
+      res = res;
+    }else{res = undefined;};
     
-    return result;
+    return res;
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+    let result = this.getUnit(initUnit).toLowerCase();
+    return change[real.indexOf(result)];
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+    index = real.indexOf(unit);
+    // console.log([inUnit[index], outUnit[index]]);
+    return [inUnit[index], outUnit[index]];
   };
   
   this.convert = function(initNum, initUnit) {
@@ -29,12 +61,34 @@ function ConvertHandler() {
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
+    switch (initUnit){
+      case "gal":
+        result = initNum * galToL;
+        break;
+      case "L":
+        result = initNum / galToL;
+        break;
+      case "lbs":
+        result = initNum * lbsToKg;
+        break;
+      case "kg":
+        result = initNum / lbsToKg;
+        break;
+      case "mi":
+        result = initNum * miToKm;
+        break;
+      case "km":
+        result = initNum / miToKm;
+        break;
+      default:
+            }
     
+    console.log(result);
     return result;
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
+    let result = `${initNum} ${inUnit[index]} converts to ${returnNum} ${outUnit[index]}`;
     
     return result;
   };
